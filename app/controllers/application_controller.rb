cc10c8633df114bda16b724d3c356221aa12eb7b
michellebonat <michelle.bonat@gmail.com>
2017-03-23 15:05:09 -0700

    Makes tweets available in views using an instance variable, via before_filter in the App controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :load_tweets
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def load_tweets
+    @tweets = Twitter.user_timeline[0..4] # Keeps the tweets limited to first 5 available.
   end
 
 end
