0fad4c58db9592309d6de7bdb2a21d3bd9bb3143
michellebonat <michelle.bonat@gmail.com>
2017-03-24 10:31:50 -0700

    Change twitter cred to ENV variables.

--- config/initializers/twitter_credentials.rb
+++ config/initializers/twitter_credentials.rb
@@ -1,5 +1,8 @@
 require 'twitter'
 
-Twitter.configure do |config|
-
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.access_token = ENV['YOUR_ACCESS_TOKEN']
+  config.access_token_secret = ENV['YOUR_ACCESS_SECRET']
 end
