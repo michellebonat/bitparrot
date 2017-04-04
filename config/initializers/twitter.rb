b82a0c77c1c8d5f08a324575d1f5ee9111544ac8
michellebonat <michelle.bonat@gmail.com>
2017-04-04 13:54:29 -0700

    Renders tweets locally onto home page from third party tweeter, using twitter API.

--- config/initializers/twitter.rb
+++
@@ -1,9 +0,0 @@
-# require 'twitter'
-
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key = ENV['CONFIG_CONSUMER_KEY']
-  config.consumer_secret = ENV['CONFIG_CONSUMER_SECRET']
-  config.access_token = ENV['CONFIG_ACCESS_TOKEN']
-  config.access_token_secret = ENV['CONFIG_ACCESS_TOKEN_SECRET']
-end
-
---
+++ lib/twitter_api.rb
@@ -0,0 +1,13 @@
+class TwitterApi
+    def self.public_tweets
+        client.user_timeline('rubyonrailsnews', count: 1, exclude_replies: true, include_rts: false)
+    end
+
+    def self.client
+        @client ||= Twitter::REST::Client.new do |config|
+            config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+            config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+        end
+    end
+end
+
