2fe2b2fd21ec3463240a0b2bd027189bc0604b6d
michellebonat <michelle.bonat@gmail.com>
2017-04-05 11:33:59 -0700

    Open clicked links in new window.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     _parsed_tweet = tweet.text.dup
 
     tweet.urls.each do |entity|
-      html_link = link_to(entity.display_url.to_s, entity.expanded_url.to_s)
+      html_link = link_to(entity.display_url.to_s, entity.expanded_url.to_s, target: '_blank')
       _parsed_tweet.sub!(entity.url.to_s, html_link)
     end
   end
