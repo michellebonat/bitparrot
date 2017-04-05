8d7dfe63d62a79c022eac91584d7ffd41488974c
michellebonat <michelle.bonat@gmail.com>
2017-04-05 12:15:22 -0700

    Update helper to also parse urls in tweets for media links, same as we did for other types of urls.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,11 @@ module ApplicationHelper
       _parsed_tweet.sub!(entity.url.to_s, html_link)
     end
 
+    tweet.media.each do |entity|
+      html_link = link_to(entity.display_url.to_s, entity.expanded_url.to_s, target: '_blank')
+      _parsed_tweet.sub!(entity.url.to_s, html_link)
+    end
+
     _parsed_tweet.html_safe
   end
 end
