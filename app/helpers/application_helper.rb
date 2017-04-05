62f5128d97164fa440cdda2573f79df92aafc4b6
michellebonat <michelle.bonat@gmail.com>
2017-04-05 11:32:34 -0700

    Loop through tweet urls, substitute the url with an html link, and link out to the expanded url, as strings.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   def parsed_tweet tweet
     _parsed_tweet = tweet.text.dup
+
+    tweet.urls.each do |entity|
+      html_link = link_to(entity.display_url.to_s, entity.expanded_url.to_s)
+      _parsed_tweet.sub!(entity.url.to_s, html_link)
+    end
   end
 end
