30c91aaddaa963f7f4522886546f16ced70872be
michellebonat <michelle.bonat@gmail.com>
2017-04-05 15:18:50 -0700

    Change autoload to eager_load to fix production rendering issue, since this is a rails 5 app.

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Bitparrot
   class Application < Rails::Application
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
 
     config.generators do |g|
       g.test_framework :rspec,
