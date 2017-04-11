4a993dfbf6041a8f49a40039b2e0a25df3ddda37
michellebonat <michelle.bonat@gmail.com>
2017-04-10 18:12:32 -0700

    Update how using httparty and authenticating, now can pull Data Simply data from API in console, using setup with Env variable.

--- lib/datasimply_api.rb
+++ lib/datasimply_api.rb
@@ -1,59 +1,20 @@
 require 'httparty'
 
-class DataSimplyApi
+class DatasimplyApi
     include HTTParty
 
     base_uri'https://api.datasimply.com/v1'
     format :json
 
-    def initialize(token)
-        @authentication = {Bearer: token }
-    end
-
-    attr_accessor :filings, :tags, :concepts
-
-    def initialize(filings, tags, concepts)
-      self.filings = filings
-      self.tags = tags
-      self.concepts = concepts
-    end
-
-    # stubbing out the various find methods they are not yet working
-
-    # Returns filings for the most recent 20
-    def self.find(filings)
-      response = get("/sec_filings.json") # need to restrict how many are pulled how to do that?
-      if response.success?
-        self.new(response["filings"])
-      else
-        raise response.response
-      end
+    # Returns filings for the most recent 20 by design of API
+    def self.find_filings
+      response = get("/sec_filings.json", :headers => { "Authorization" => "Bearer #{ENV['DATASIMPLY_TOKEN']}"})
+      puts response.inspect
     end
 
     # Returns tags for the most recent 20 filings
-    def self.find(tags)
-      response = get("/tags.json") # need to restrict how many are pulled how to do that?
-      if response.success?
-        self.new(response["tags"])
-      else
-        raise response.response
-      end
-    end
 
     # Returns concepts for the most recent 20 filings
-    def self.find(concepts)
-      response = get("/concepts.json") # need to restrict how many are pulled how to do that?
-      if response.success?
-        self.new(response["concepts"])
-      else
-        raise response.response
-      end
-    end
 
-    def self.client
-        @client ||= DataSimply::REST::Client.new do |config|
-            token        = ENV['DATASIMPLY_TOKEN']
-        end
-    end
 end
 
