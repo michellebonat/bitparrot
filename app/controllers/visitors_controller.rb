e332c59771bdc6a927f3b1c09a451f81d7f660f1
michellebonat <michelle.bonat@gmail.com>
2017-09-20 14:10:56 -0700

    Nav, routes, and controller method for new ROR page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,10 @@ class VisitorsController < ApplicationController
     @meta_title = "Bit Parrot"
   end
 
+  def ruby_and_rails
+    @meta_title = "Ruby and Rails Developer Info"
+  end
+
   def python
     @meta_title = "Python Developer Info"
   end
